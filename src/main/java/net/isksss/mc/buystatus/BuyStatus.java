68a9cb28f53b98984fbe8025be880655a7af5932
isksss <104404522+isksss@users.noreply.github.com>
2024-01-22 15:14:20 +0900

    fix

--- src/main/java/net/isksss/mc/buystatus/BuyStatus.java
+++ src/main/java/net/isksss/mc/buystatus/BuyStatus.java
@@ -35,7 +35,7 @@ public final class BuyStatus extends JavaPlugin {
             return false;
         }
         econ = rsp.getProvider();
-        return econ != null;
+        return true;
     }
 
     public static Economy getEconomy() {
